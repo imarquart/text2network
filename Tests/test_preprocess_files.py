44472f7f20899f7ef674c643e65db2658757cb3b
IngoMarquart <ingo.marquart@esmt.org>
2019-11-18 11:43:27 +0100

    struggling with python folders

--- Tests/test_preprocess_files.py
+++ Tests/test_preprocess_files.py
@@ -3,7 +3,6 @@ import os
 import time
 from NLP.Experiments.preprocess_files import pre_process_sentences_COCA
 #os.chdir('/home/ingo/PhD/BERT-NLP/BERTNLP')
-#os.chdir('/home/ingo/PhD/BERT-NLP/BERTNLP')
 start_time = time.time()
 cwd= os.getcwd()
 files=[os.path.join(cwd,'NLP/data/w_news_1990.txt')]
