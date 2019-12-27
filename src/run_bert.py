7b9526f12f4874d84cebef3f2892afba0c30e12a
Ingo <ingo.marquart@esmt.org>
2019-12-27 15:08:09 +0100

    Fix some erros in two stage, re-org BERT training file

--- src/run_bert.py
+++ src/run_bert.py
@@ -53,6 +53,64 @@ MODEL_CLASSES = {
 }
 
 
+class bert_args():
+    def __init__(self,train_data_file, output_dir, do_train,model_dir,mlm_probability=0.15, block_size=-1, gpu_batch=4,epochs=1,warmup_steps=0):
+        self.train_data_file=train_data_file
+        self.eval_data_file=train_data_file
+        self.output_dir=output_dir
+
+        self.mlm=True
+        self.mlm_probability=mlm_probability
+
+        if do_train == True:
+            self.do_train = True
+            self.do_eval = False
+        else:
+            self.do_train = False
+            self.do_eval = True
+
+        self.do_lower_case = True
+
+        self.model_name_or_path=model_dir
+
+        self.warmup_steps = warmup_steps
+        self.num_train_epochs = epochs
+
+        self.block_size=block_size
+
+        self.evaluate_during_training=True
+
+        self.per_gpu_train_batch_size=gpu_batch
+        self.per_gpu_eval_batch_size=gpu_batch
+
+        self.model_type="bert"
+        self.config_name = ""
+        self.tokenizer_name = ""
+        self.cache_dir = ""
+        self.gradient_accumulation_steps=1
+        self.learning_rate=5e-5
+        self.weight_decay=0
+        self.data_epsilon=1e-8
+        self.max_grad_norm=1.0
+        self.max_steps=-1
+        self.logging_steps=50
+        self.save_steps=50
+        self.save_total_limit=None
+        self.eva_all_checkpoints=True
+        self.no_cuda=False
+        self.overwrite_output_dir=True
+        self.overwrite_cache=True
+        self.seed=42
+        self.fp16=False
+        self.fp16_opt_level="01"
+        self.local_rank=-1
+        self.n_gpu=1
+        self.server_ip=''
+        self.server_port=''
+
+        self.device=None
+
+
 class TextDataset(Dataset):
     def __init__(self, tokenizer, args, file_path='train', block_size=30):
         assert os.path.isfile(file_path)
@@ -375,105 +433,10 @@ def evaluate(args, model, tokenizer, prefix=""):
 
     return result
 
+def run_bert(args):
 
-def main():
-    parser = argparse.ArgumentParser()
+    #
 
-    ## Required parameters
-    parser.add_argument("--train_data_file", default=None, type=str, required=True,
-                        help="The input training data file (a text file).")
-    parser.add_argument("--output_dir", default=None, type=str, required=True,
-                        help="The output directory where the model predictions and checkpoints will be written.")
-
-    ## Other parameters
-    parser.add_argument("--eval_data_file", default=None, type=str,
-                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
-
-    parser.add_argument("--model_type", default="bert", type=str,
-                        help="The model architecture to be fine-tuned.")
-    parser.add_argument("--model_name_or_path", default="bert-base-cased", type=str,
-                        help="The model checkpoint for weights initialization.")
-
-    parser.add_argument("--mlm", action='store_true',
-                        help="Train with masked-language modeling loss instead of language modeling.")
-    parser.add_argument("--mlm_probability", type=float, default=0.15,
-                        help="Ratio of tokens to mask for masked language modeling loss")
-
-    parser.add_argument("--config_name", default="", type=str,
-                        help="Optional pretrained config name or path if not the same as model_name_or_path")
-    parser.add_argument("--tokenizer_name", default="", type=str,
-                        help="Optional pretrained tokenizer name or path if not the same as model_name_or_path")
-    parser.add_argument("--cache_dir", default="", type=str,
-                        help="Optional directory to store the pre-trained models downloaded from s3 (instread of the default one)")
-    parser.add_argument("--block_size", default=-1, type=int,
-                        help="Optional input sequence length after tokenization."
-                             "The training dataset will be truncated in block of this size for training."
-                             "Default to the model max input length for single sentence inputs (take into account special tokens).")
-    parser.add_argument("--do_train", action='store_true',
-                        help="Whether to run training.")
-    parser.add_argument("--do_eval", action='store_true',
-                        help="Whether to run eval on the dev set.")
-    parser.add_argument("--evaluate_during_training", action='store_true',
-                        help="Run evaluation during training at each logging step.")
-    parser.add_argument("--do_lower_case", action='store_true',
-                        help="Set this flag if you are using an uncased model.")
-
-    parser.add_argument("--per_gpu_train_batch_size", default=4, type=int,
-                        help="Batch size per GPU/CPU for training.")
-    parser.add_argument("--per_gpu_eval_batch_size", default=4, type=int,
-                        help="Batch size per GPU/CPU for evaluation.")
-    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.")
-    parser.add_argument("--learning_rate", default=5e-5, type=float,
-                        help="The initial learning rate for Adam.")
-    parser.add_argument("--weight_decay", default=0.0, type=float,
-                        help="Weight deay if we apply some.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
-                        help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float,
-                        help="Max gradient norm.")
-    parser.add_argument("--num_train_epochs", default=1.0, type=float,
-                        help="Total number of training epochs to perform.")
-    parser.add_argument("--max_steps", default=-1, type=int,
-                        help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
-    parser.add_argument("--warmup_steps", default=0, type=int,
-                        help="Linear warmup over warmup_steps.")
-
-    parser.add_argument('--logging_steps', type=int, default=50,
-                        help="Log every X updates steps.")
-    parser.add_argument('--save_steps', type=int, default=50,
-                        help="Save checkpoint every X updates steps.")
-    parser.add_argument('--save_total_limit', type=int, default=None,
-                        help='Limit the total amount of checkpoints, delete the older checkpoints in the output_dir, does not delete by default')
-    parser.add_argument("--eval_all_checkpoints", action='store_true',
-                        help="Evaluate all checkpoints starting with the same prefix as model_name_or_path ending and ending with step number")
-    parser.add_argument("--no_cuda", action='store_true',
-                        help="Avoid using CUDA when available")
-    parser.add_argument('--overwrite_output_dir', action='store_true',
-                        help="Overwrite the content of the output directory")
-    parser.add_argument('--overwrite_cache', action='store_true',
-                        help="Overwrite the cached training and evaluation sets")
-    parser.add_argument('--seed', type=int, default=42,
-                        help="random seed for initialization")
-
-    parser.add_argument('--fp16', action='store_true',
-                        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
-    parser.add_argument('--fp16_opt_level', type=str, default='O1',
-                        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-                             "See details at https://nvidia.github.io/apex/amp.html")
-    parser.add_argument("--local_rank", type=int, default=-1,
-                        help="For distributed training: local_rank")
-    parser.add_argument('--server_ip', type=str, default='', help="For distant debugging.")
-    parser.add_argument('--server_port', type=str, default='', help="For distant debugging.")
-    args = parser.parse_args()
-
-    if args.model_type in ["bert", "roberta", "distilbert", "camembert"] and not args.mlm:
-        raise ValueError("BERT and RoBERTa do not have LM heads but masked LM heads. They must be run using the --mlm "
-                         "flag (masked language modeling).")
-    if args.eval_data_file is None and args.do_eval:
-        raise ValueError(
-            "Cannot do evaluation without an evaluation data file. Either supply a file to --eval_data_file "
-            "or remove the --do_eval argument.")
 
     if os.path.exists(args.output_dir) and os.listdir(
             args.output_dir) and args.do_train and not args.overwrite_output_dir:
@@ -486,16 +449,11 @@ def main():
     # Disable dist
     args.local_rank = -1
 
-    if args.local_rank == -1 or args.no_cuda:
-        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
-        #args.n_gpu = torch.cuda.device_count()
-        args.n_gpu =1
 
-    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-        torch.cuda.set_device(args.local_rank)
-        device = torch.device("cuda", args.local_rank)
-        torch.distributed.init_process_group(backend='nccl')
-        args.n_gpu = 1
+    device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    #args.n_gpu = torch.cuda.device_count()
+    args.n_gpu =1
+
     args.device = device
 
     # Setup logging
@@ -518,13 +476,11 @@ def main():
     #tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
     #                                            do_lower_case=args.do_lower_case,
     #                                            cache_dir=args.cache_dir if args.cache_dir else None)
-    modelpath="D:/NLP/BERT-NLP/NLP/models"
-    tokenizer, model = get_bert_and_tokenizer_local(modelpath)
+
+    tokenizer, model = get_bert_and_tokenizer_local(args.model_dir)
     if args.block_size <= 0:
         args.block_size = tokenizer.max_len_single_sentence  # Our input block size will be the max possible for the model
     args.block_size = min(args.block_size, tokenizer.max_len_single_sentence)
-    #model = model_class.from_pretrained(args.model_name_or_path,
-    #                                    config=config)
     model.to(args.device)
 
     if args.local_rank == 0:
@@ -588,6 +544,3 @@ def main():
 
     return results
 
-
-if __name__ == "__main__":
-    main()
