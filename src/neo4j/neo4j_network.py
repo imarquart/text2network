ba507ec58192bf259229ee894c63465ea303de6d
Ingo <ingo.marquart@esmt.org>
2020-03-26 18:10:02 +0100

    Test class, setup and tests for insert/retrieve and setup

--- src/neo4j/neo4j_network.py
+++ src/neo4j/neo4j_network.py
@@ -63,17 +63,19 @@ class neo4j_network(MutableSequence):
         pass
 
     #%% Setup
-    def setup_neo_db(self,tokens,token_ids,years):
+    def setup_neo_db(self,tokens,token_ids):
+        """Creates tokens and token_ids in Neo database"""
+        logging.info("Creating indecies and nodes in Neo4j database.")
         # Create uniqueness constraints
-        query=["CREATE CONSTRAINT ON(n:word) ASSERT n.token_id IS UNIQUE"]
+        query="CREATE CONSTRAINT ON(n:word) ASSERT n.token_id IS UNIQUE"
         self.add_query(query)
-        query=["CREATE CONSTRAINT ON(n:word) ASSERT n.token IS UNIQUE"]
+        query="CREATE CONSTRAINT ON(n:word) ASSERT n.token IS UNIQUE"
         self.add_query(query)
         # Need to write first because create and structure changes can not be batched
         self.write_queue()
         # Create nodes in neo db
-        queries=[''.join(["CREATE (n:word {token_id: ",str(id),", token: '",tok,"'})"]) for tok,id in zip(tokens,token_ids)]
-        self.add_query(queries)
+        queries=[''.join(["MERGE (n:word {token_id: ",str(id),", token: '",tok,"'})"]) for tok,id in zip(tokens,token_ids)]
+        self.add_queries(queries)
         self.write_queue()
 
 
@@ -88,55 +90,77 @@ class neo4j_network(MutableSequence):
 
     #%% Neo4J interaction
     def add_query(self,query,params=None):
+        self.add_queries([query],[params])
+
+    def add_queries(self,query,params=None):
+        assert isinstance(query,list)
+
         if params is not None:
-            self.neo_queue.extend(neo4j.Statement(query,params))
+            statements=[neo4j.Statement(q,p) for (q,p) in zip(query,params)]
+            self.neo_queue.extend(statements)
         else:
-            self.neo_queue.extend(neo4j.Statement(query))
+            statements = [neo4j.Statement(q) for (q) in query]
+            self.neo_queue.extend(statements)
 
     def write_queue(self):
         # This may or may not be optimized by using parameters and individual queries.
         res=self.connector.run_multiple(self.neo_queue, self.neo_batch_size)
         self.neo_queue=[]
 
-    def query_node(self,id, years=None):
-        return self.query_multiple_nodes([id],years)
+    def query_node(self,id, times=None):
+        return self.query_multiple_nodes([id],times)
 
-    def query_multiple_nodes(self, ids, years=None):
+    def query_multiple_nodes(self, ids, times=None):
         assert isinstance(ids,list)
-        if years is not None:
-            assert isinstance(years,list)
-            params = {"ids": ids, "years":years}
-            query="UNWIND $years as year UNWIND $ids AS id MATCH p=(a:word)-[r:PREDICTS {year: year}]->(b:word  {token_id:id}) RETURN b.token_id,a.token_id,r.year AS year,r.weight AS weight"
-        else:
-            query="unwind $ids AS id MATCH p=(a:word)-[r:PREDICTS]->(b:word  {token_id:id}) RETURN b.token_id,a.token_id,r.year AS year,r.weight AS weight"
-            params={"ids":ids}
-
+        if self.graph_direction == "PREDICTED": # Seek nodes that predict ID nodes hence reverse sender/receiver
+            if times is not None:
+                assert isinstance(times,list)
+                params = {"ids": ids, "times":times}
+                query="UNWIND times as time UNWIND $ids AS id MATCH p=(a:word)-[r:PREDICTS {time: time}]->(b:word  {token_id:id}) RETURN b.token_id AS sender,a.token_id AS receiver,r.time AS time,r.weight AS weight"
+            else:
+                query="unwind $ids AS id MATCH p=(a:word)-[r:PREDICTS]->(b:word  {token_id:id}) RETURN b.token_id AS sender,a.token_id AS receiver,r.time AS time,r.weight AS weight"
+                params={"ids":ids}
+        else: # Seek nodes that predict nodes
+            if times is not None:
+                assert isinstance(times,list)
+                params = {"ids": ids, "times":times}
+                query="UNWIND times as time UNWIND $ids AS id MATCH p=(a:word)<-[r:PREDICTS {time: time}]-(b:word  {token_id:id}) RETURN b.token_id AS sender,a.token_id as receiver,r.time AS time,r.weight AS weight"
+            else:
+                query="unwind $ids AS id MATCH p=(a:word)<-[r:PREDICTS]-(b:word  {token_id:id}) RETURN b.token_id AS sender,a.token_id as receiver,r.time AS time,r.weight AS weight"
+                params={"ids":ids}
         res=self.connector.run(query,params)
-        if self.graph_direction=="PREDICTED":
-            ties=[(x['b.token_id'], x['a.token_id'], x['year'], {'weight': x['weight']}) for x in res]
-        else:
-            ties = [(x['a.token_id'], x['b.token_id'], x['year'], {'weight': x['weight']}) for x in res]
+        ties = [(x['sender'], x['receiver'], x['time'], {'weight': x['weight']}) for x in res]
         return ties
 
     def insert_edges_query_multiple(self,ties):
-
+        """
+        Allows to add ties across nodes
+        :param ties: Set of Tuples (u,v,Time,{"weight":x})
+        :return:
+        """
         # Graph is saved as "PREDICTS", thus we may need to reverse ego/alter here
         if self.graph_direction=="PREDICTED":
-            params={"ties": [{"ego":x[1],"alter":x[0],"year":x[2],"weight":x[3]['weight']} for x in ties]}
+            params={"ties": [{"ego":x[1],"alter":x[0],"time":str(x[2]),"weight":x[3]['weight']} for x in ties]}
         else:
-            params={"ties": [{"ego":x[0],"alter":x[1],"year":x[2],"weight":x[3]['weight']} for x in ties]}
+            params={"ties": [{"ego":x[0],"alter":x[1],"time":str(x[2]),"weight":x[3]['weight']} for x in ties]}
 
-        query="UNWIND $ties AS tie MATCH (a:word {token_id: tie.ego}) MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)-[:PREDICTS {weight:tie.weight, year:tie.year}]->(b)"
+        query="UNWIND $ties AS tie MATCH (a:word {token_id: tie.ego}) MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)-[:PREDICTS {weight:tie.weight, time:date(tie.time)}]->(b)"
         self.add_query(query,params)
 
     def insert_edges_query(self,ego,ties):
+        """
+        Add ties from a given ego node u->v
+        TODO: Refactor into above function
+        :param ties: Set of Tuples (v,Time,{"weight":x})
+        :return:
+        """
         # Graph is saved as "PREDICTS", thus we may need to reverse ego/alter here
         if self.graph_direction=="PREDICTED":
-            params = {"ties": [{"alter": x[0], "year": x[1], "weight": x[2]['weight']} for x in ties], "ego": ego}
-            query = "MATCH (a:word {token_id: $ego}) WITH a UNWIND $ties AS tie MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)<-[:PREDICTS {weight:tie.weight, year:tie.year}]-(b)"
+            params = {"ties": [{"alter": x[0], "time": str(x[1]), "weight": x[2]['weight']} for x in ties], "ego": ego}
+            query = "MATCH (a:word {token_id: $ego}) WITH a UNWIND $ties AS tie MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)<-[:PREDICTS {weight:tie.weight, time:date(tie.time)}]-(b)"
         else:
-            params={"ties": [{"alter":x[0],"year":x[1],"weight":x[2]['weight']} for x in ties], "ego":ego}
-            query="MATCH (a:word {token_id: $ego}) WITH a UNWIND $ties AS tie MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)-[:PREDICTS {weight:tie.weight, year:tie.year}]->(b)"
+            params={"ties": [{"alter":x[0],"time":str(x[1]), "weight": x[2]['weight']} for x in ties], "ego":ego}
+            query="MATCH (a:word {token_id: $ego}) WITH a UNWIND $ties AS tie MATCH (b:word {token_id: tie.alter}) WITH a,b,tie MERGE (a)-[:PREDICTS {weight:tie.weight, time:date(tie.time)}]->(b)"
         self.add_query(query,params)
 
 
@@ -166,176 +190,3 @@ class neo4j_network(MutableSequence):
         # Update dictionaries
         self.token_id_dict.update(dict(zip(self.tokens,self.ids)))
 
-
-
-
-
-
-class neo4j_network_old(MutableSequence):
-
-
-    def init_test_graph(self):
-        """Just add a test graph from neo data"""
-        self.graph=nx.MultiDiGraph()
-        self.graph.add_nodes_from(self.neo_ids)
-        self.graph_tokens=self.neo_tokens
-        self.graph_ids = list(self.graph.nodes)
-        self.update_graph_dicts()
-
-    def init_neo4j_tokens(self):
-        """Init neo4j"""
-        self.neo_tokens = np.array(["Car", "House", "Window"])
-        self.neo_ids = np.array([3,4,5])
-        self.update_neo_dicts()
-
-    #%% Utility functions
-
-    def get_index_from_token(self,token):
-        """Index (order) of token in data structures used"""
-        # Token has to be string
-        assert isinstance(token, str)
-        if self.graph is None: # No graph initialized
-            index=np.flatnonzero(self.neo_tokens == token)
-            if index.size == 0:
-                index=False
-        else: # graph initialized, use our dicts
-            try:
-                id=self.token_id_dict[token]
-                index=self.id_index_dict[id]
-            except:
-                index=False
-        return index.item()
-
-    def get_id_from_token(self,token):
-        """Get the token id"""
-        # Token has to be string
-        assert isinstance(token, str)
-        if self.graph is None: # No graph initialized
-            try:
-                id=self.neo_token_id_dict[token]
-            except:
-                id=False
-        else: # graph initialized, use our dicts
-            try:
-                id=self.token_id_dict[token]
-            except:
-                # token not in graph, infer from neo4j
-                try:
-                    id = self.neo_token_id_dict[token]
-                except:
-                    id = False
-        return id
-
-    def get_token_from_id(self,id):
-        """Get the token assigned to id"""
-        # id has to be integer
-        assert isinstance(id, int)
-        if self.graph is None: # No graph initialized
-            try:
-                token=self.neo_token_id_dict[id]
-            except:
-                token="UNK"
-        else: # graph initialized, use our dicts
-            try:
-                token=self.token_id_dict[id]
-            except:
-                # Failed to find in graph, infer from neo4j
-                try:
-                    token = self.neo_token_id_dict[id]
-                except:
-                    token = "UNK"
-        return token
-
-    def add_neo_queue(self,add):
-        self.neo_queue.append(add)
-
-    #%% Delete / Update functions
-
-    def write_neo4j_query(self):
-        # TODO: Write query
-        pass
-
-    def update_graph_dicts(self):
-        """Use graph properties to update dicts"""
-        if self.graph is not None:
-            indecies = list(range(0, len(self.graph_ids)))
-            # Update dictionaries
-            self.token_id_dict.update(dict(zip(self.graph_tokens,self.graph_ids)))
-            self.id_index_dict.update(dict(zip(self.graph_ids,indecies)))
-
-    def update_neo_dicts(self):
-        """Simply update dictionaries"""
-        # Update dictionaries
-        self.neo_token_id_dict.update(dict(zip(self.neo_tokens,self.neo_ids)))
-
-    #%% Adding Functions
-
-    def __setitem__(self, key, value):
-        """Interpret as adding ties in the graph"""
-        # We work with token id's here
-        if isinstance(key, str): key = self.get_id_from_token(key)
-        try:
-            neighbors,wy = zip(*value)
-            weights, years = zip(*wy)
-            neighbors=[self.get_id_from_token(x) if not isinstance(x,int) else x for x in neighbors]
-            neighbors=np.array(neighbors)
-            weights=np.array(weights)
-            years = np.array(years)
-            token = np.repeat(key,len(neighbors))
-        except:
-            raise ValueError("Adding requires an iterable over tuples e.g. [(neighbor,(weight,year))]")
-
-        for y in np.unique(years):
-            mask=years==y
-            self.add_ties(token[mask],neighbors[mask],weights[mask], years[mask][0])
-
-    def add_ties(self, token_ids,neighbors,weights,year):
-        """This function adds ties, checking if nodes need to be added"""
-        if self.graph is not None:
-            add_ids=token_ids[~np.isin(token_ids,self.graph_ids)]
-            add_neighbors = neighbors[~np.isin(neighbors, self.graph_ids)]
-            add_ids=np.union1d(add_ids,add_neighbors)
-            if add_ids.size > 0:
-                add_token = np.array([self.get_token_from_id(np.int(x)) for x in add_ids],dtype=np.str)
-                self.insert(add_token,add_ids)
-            data=[{"weight":x} for x in weights]
-            edges=zip(token_ids,neighbors,np.repeat(year,len(neighbors)),data)
-            self.graph.add_edges_from(edges)
-        # Add to neo queue
-        self.add_neo_queue("Add ties")
-
-    def insert(self, token, token_id):
-        """Basic function to add a token and its token id"""
-        if self.graph is not None:
-            self.graph.add_nodes_from(token_id)
-            self.graph_ids=np.append(self.graph_ids,token_id)
-            self.graph_tokens=np.append(self.graph_tokens,token)
-            self.update_graph_dicts()
-        ## Add to neo queue
-        # First add internally, then update queue
-        self.neo_tokens=np.append(self.neo_tokens,token)
-        self.neo_ids = np.append(self.neo_ids, token_id)
-        self.update_neo_dicts()
-        self.add_neo_queue("Add nodes")
-
-    #%% Query functions
-
-    def __getitem__(self, i):
-        """Main function to return token information"""
-        # We work with token id's here
-        if isinstance(i,str): i = self.get_id_from_token(i)
-        if self.graph is not None: # Graph initialized
-            return self.graph[i]
-        else: # Use neo4j
-            # Query neo4j
-            return self.neo_token_id_dict[i]
-
-    def __len__(self):
-        # Since all tokens are accessible via query, return the number of tokens in DB
-        if self.graph is None:
-            return len(self.neo_tokens)
-        else:
-            return len(self.graph_tokens)
-
-
-asdf=neo4j_network(None, None)
--- src/neo4j/test_neo4j_network.py
+++ src/neo4j/test_neo4j_network.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from NLP.src.neo4j import neo4j_network
+from NLP.src.neo4j.neo4j_network import neo4j_network
 
 class Testneo4j_network(TestCase):
 
@@ -7,20 +7,37 @@ class Testneo4j_network(TestCase):
         db_uri="http://localhost:7474"
         db_pwd=('neo4j','nlp')
         graph_type="networkx"
-        tokens=["car","house","dog","cat"]
-        token_ids=[10,11,12,13]
-        add_string=[('house', (1, 2000)), ('dog', (0.6, 2000))]
-        add_string_new=[('house', (1, 2000)), ('bird', (0.5, 2000))]
-
-        neo4nw=neo4j_network((db_uri,db_pwd),graph_type)
-
+        self.tokens=["car","house","dog","cat"]
+        self.token_ids=[10,11,12,13]
+        self.ego_id=11
+        self.add_string=[(11, 12, 20000101, {'weight': 0.5}), (11, 13, 20000101, {'weight': 0.5}), (12, 13, 20000101, {'weight': 0.5})]
+        self.add_string_ego=[(12, 20000101, {'weight': 0.5}), (13, 20000101, {'weight': 0.5})]
 
 
-    def test_initialize_connection(self):
-        pass
+        self.neo4nw=neo4j_network((db_uri,db_pwd),graph_type)
+        self.setup_network()
+
+    def setup_network(self):
+        """Delete network and re-create it """
+        # Run deletion query
+        query="MATCH (n) DETACH DELETE n"
+        self.neo4nw.connector.run(query)
+        # Setup network
+        self.neo4nw.setup_neo_db(self.tokens,self.token_ids)
+
+
+
+    def test_setup(self):
+        # Test
+        query = "MATCH (n) RETURN n.token_id AS id"
+        res = self.neo4nw.connector.run(query)
+        id_list = [x['id'] for x in res]
+        self.assertTrue(set(id_list) == set(self.token_ids))
 
     def test_init_tokens(self):
-        pass
+        self.neo4nw.init_tokens()
+        self.assertTrue(set(self.neo4nw.ids) == set(self.token_ids))
+        self.assertTrue(set(self.neo4nw.tokens) == set(self.tokens))
 
     def test_get_token_from_db(self):
         pass
@@ -29,6 +46,7 @@ class Testneo4j_network(TestCase):
         pass
 
     def test_confirm_stack_write(selfs):
+        pass
 
     def test_add_token(self):
         pass
@@ -36,20 +54,62 @@ class Testneo4j_network(TestCase):
     def test_update_token(self):
         pass
 
-    def test_add_link_token(self):
+    def test_add_get_link_token(self):
         pass
 
-    def test_add_link_id(self):
-        pass
+    def test_add_get_link_id(self):
+        """We add a list of edges between 3 nodes and check it is returned correctly (same format)"""
+        # Since ties are saved as "PREDICTS", this includes reversal aka
+        # here self.ego_id=11 is ego, but in neo4j ties go towards 11
+        self.neo4nw.insert_edges_query_multiple(self.add_string)
+        self.neo4nw.write_queue()
+        res=self.neo4nw.query_node(self.ego_id)
+        res=[(x[0],x[1],x[3]['weight']) for x in res]
+        comp=[(self.ego_id,x[0],x[2]['weight']) for x in self.add_string_ego]
+        self.assertEqual(set(res),set(comp))
 
-    def test_get_link_token(self):
-        pass
+    def test_add_get_link_id_ego(self):
+        """Add two ties towards ego node"""
+        # Again, input states ties going away from self.ego_id but
+        # network should save as going toward self.ego_id
+        # and reverse when giving return query
+        self.neo4nw.insert_edges_query(self.ego_id,self.add_string_ego)
+        self.neo4nw.write_queue()
+        res=self.neo4nw.query_node(self.ego_id)
+        res=[(x[0],x[1],x[3]['weight']) for x in res]
+        comp=[(self.ego_id,x[0],x[2]['weight']) for x in self.add_string_ego]
+        self.assertEqual(set(res),set(comp))
 
-    def test_get_link_id(self):
-        pass
+    def test_add_get_link_id_ego_reverse(self):
+        """Add links via ego, reverse direction"""
+        # Now direction of class and neo4j are equal
+        self.setup_network()
+        self.neo4nw.graph_direction="PREDICTS"
+
+        self.neo4nw.insert_edges_query(self.ego_id,self.add_string_ego)
+        self.neo4nw.write_queue()
+
+        res=self.neo4nw.query_node(self.ego_id)
+        res=[(x[0],x[1],x[3]['weight']) for x in res]
+        comp=[(self.ego_id,x[0],x[2]['weight']) for x in self.add_string_ego]
+        self.assertEqual(set(res),set(comp))
+
+    def test_add_get_link_id_reverse(self):
+        """Add and retrieve list of links"""
+        # Again this is the direction as given in neo4j
+        self.neo4nw.graph_direction = "PREDICTS"
+        self.neo4nw.insert_edges_query(self.ego_id,self.add_string_ego)
+        self.neo4nw.write_queue()
+        res=self.neo4nw.query_node(self.ego_id)
+        res=[(x[0],x[1],x[3]['weight']) for x in res]
+        comp=[(self.ego_id,x[0],x[2]['weight']) for x in self.add_string_ego]
+        self.assertEqual(set(res),set(comp))
 
     def test_add_link_new_token(self):
         pass
 
     def test_get_links_missing_ego(self):
         pass
+
+#if __name__ == '__main__':
+#    unittest.main()
