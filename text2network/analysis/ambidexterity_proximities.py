d484990b126473d4b473a12e81cb66977537bd59
Ingo <ingo.marquart@esmt.org>
2021-09-10 16:43:56 +0200

    ambidextrous analyses

---
+++ text2network/analysis/ambidexterity_proximities.py
@@ -0,0 +1,86 @@
+import time
+from itertools import product
+
+from text2network.functions.file_helpers import check_create_folder
+from text2network.measures.measures import average_cluster_proximities, extract_all_clusters, proximities
+from text2network.utils.logging_helpers import setup_logger
+import logging
+import numpy as np
+from text2network.classes.neo4jnw import neo4j_network
+
+# Set a configuration path
+configuration_path = '/config/config.ini'
+# Settings
+years = list(range(1980, 2021))
+alter_subset = None
+# Load Configuration file
+import configparser
+config = configparser.ConfigParser()
+print(check_create_folder(configuration_path))
+config.read(check_create_folder(configuration_path))
+# Setup logging
+setup_logger(config['Paths']['log'], config['General']['logging_level'], "ambidexterity_proximities.py")
+
+
+import os
+
+os.environ['NUMEXPR_MAX_THREADS'] = '16'
+# First, create an empty network
+semantic_network = neo4j_network(config)
+
+weight_list = [0]
+rs_list = [100]
+batch_sizes=[1000]
+rev_ties_list =[True,False]
+sym_ties_list =[True,False]
+comp_ties_list = [True,False]
+back_out_list = [False]
+focal_context_list = [("agile",None),("agility",None)]
+
+param_list = product(rs_list, weight_list,focal_context_list,rev_ties_list,back_out_list,comp_ties_list, sym_ties_list)
+logging.info("------------------------------------------------")
+for rs, cutoff, fc_list, rev,backout,comp, sym  in param_list:
+    focal_token, context = fc_list
+    ##
+    if rev:
+        depth=1
+    else:
+        depth=0
+
+    start_time = time.time()
+
+    del semantic_network
+    np.random.seed(rs)
+    semantic_network = neo4j_network(config)
+    filename = "".join(
+        [config['Paths']['csv_outputs'], "/Prox_", str(focal_token), "_backout",
+         str(backout), "_context", str(context is not None), "_sym", str(sym),"_rev", str(rev), "_norm",
+         str(comp), "_cut",
+         str(cutoff), "_rs", str(rs),".xlsx"])
+    logging.info("Focal token:{}, {}".format(focal_token, filename))
+    semantic_network.condition(tokens=focal_token, times=None, context=context, depth=depth, weight_cutoff=cutoff,
+                 compositional=comp)
+    if rev:
+        semantic_network.to_reverse()
+    if sym:
+        semantic_network.to_symmetric()
+
+    df = proximities(semantic_network, focal_tokens=focal_token)
+    df = semantic_network.pd_format(df)[0].T
+
+    # This was the difference between founder and CEO, redo this elsewhere
+    #df.iloc[:, 0]=df.iloc[:,0]/np.sum(df.iloc[:,0])
+    #df.iloc[:, 1]=df.iloc[:,1]/np.sum(df.iloc[:,1])
+    #df["diff"]=df.iloc[:,0]-df.iloc[:,1]
+    #df["abs_diff"]=np.abs(df.iloc[:,0]-df.iloc[:,1])
+    #df["sum"]=df.iloc[:,0]+df.iloc[:,1]
+    #df["relative_diff"]=df["diff"]/df["sum"]
+    #df["abs_rel_diff"]=df["abs_diff"]/df["sum"]
+    #df["sum_same"]=df["sum"]/df["abs_diff"]
+    #print(df)
+    filename = check_create_folder(filename)
+    logging.info("Time:{}".format(time.time() - start_time))
+    df.to_excel(filename,merge_cells=False)
+
+
+
---
+++ text2network/analysis/ambidextrous_ego_clusters.py
@@ -0,0 +1,58 @@
+from itertools import product
+
+from text2network.functions.file_helpers import check_create_folder
+from text2network.measures.measures import average_cluster_proximities, extract_all_clusters
+from text2network.utils.logging_helpers import setup_logger
+import logging
+import numpy as np
+from text2network.functions.graph_clustering import consensus_louvain, louvain_cluster
+from text2network.classes.neo4jnw import neo4j_network
+
+# Set a configuration path
+configuration_path = '/config/config.ini'
+# Settings
+years = list(range(1980, 2021))
+focal_token = "ambidextrous"
+alter_subset=None
+import os
+os.environ['NUMEXPR_MAX_THREADS'] = '16'
+alter_subset=None
+# Load Configuration file
+import configparser
+
+config = configparser.ConfigParser()
+print(check_create_folder(configuration_path))
+config.read(check_create_folder(configuration_path))
+# Setup logging
+setup_logger(config['Paths']['log'], config['General']['logging_level'], "ambidextrous_ego_clusters.py")
+
+# First, create an empty network
+semantic_network = neo4j_network(config)
+logging.info("first nw created")
+level_list = [5]
+weight_list = [0, 0.01]
+cl_clutoff_list = [0,90,0.1,95]
+depth_list = [1]
+rs_list = [100]
+rev_ties_list = [False]
+algolist=[consensus_louvain]
+alter_set=[None]
+focaladdlist=[True]
+comp_ties_list = [False]
+back_out_list= [False]
+symmetry_list=[True]
+param_list = product(depth_list, level_list, rs_list, weight_list, rev_ties_list, comp_ties_list, cl_clutoff_list,algolist,back_out_list,symmetry_list,focaladdlist,alter_set)
+logging.info("------------------------------------------------")
+for depth, level, rs, cutoff, rev, comp, cluster_cutoff,algo,backout,sym,fadd,alters in param_list:
+    del semantic_network
+    np.random.seed(rs)
+    semantic_network = neo4j_network(config)
+    filename = "".join(
+        [config['Paths']['csv_outputs'], "/EgoCluster_", str(focal_token), "_backout", str(backout),"_sym", str(sym),"_fadd", str(fadd),"_alters", str(str(isinstance(alters,list))),"_rev", str(rev), "_norm", str(comp),
+         "_lev", str(level), "_cut",
+         str(cutoff), "_clcut", str(cluster_cutoff), "_algo", str(algo.__name__), "_depth", str(depth), "_rs", str(rs)])
+    logging.info("Network clustering: {}".format(filename))
+    df = average_cluster_proximities(focal_token=focal_token, nw=semantic_network, levels=level, interest_list=alters, times=years,do_reverse=True,
+                                     depth=depth, weight_cutoff=cutoff, cluster_cutoff=cluster_cutoff, year_by_year=False, add_focal_to_clusters=fadd,
+                                     moving_average=None, filename=filename, compositional=comp, to_back_out=backout, include_all_levels=True, add_individual_nodes=True,
+                                     reverse_ties=rev, symmetric=sym, seed=rs, export_network=True)
