d3b32d31718631caef246ed44f9f6f9e25246104
Ingo <ingo.marquart@esmt.org>
2021-11-11 12:49:13 +0100

    Error in input check

--- text2network/utils/input_check.py
+++ text2network/utils/input_check.py
@@ -25,61 +25,62 @@ def input_check(times=None, tokens=None):
     times and tokens, possibly cast to basic python types
 
     """
-    if not isinstance(times, (np.ndarray, list, dict, int, np.integer)):
-        logging.error(
-            "Parameter times must be int, list, array or interval dict ('start':int,'end':int). Supplied: {}".format(
-                type(times)))
-        raise AttributeError(
-            "Parameter times must be int, list, array or interval dict ('start':int,'end':int). Supplied: {}".format(
-                type(times)))
-    else:
-        # We use lists instead of arrays here
-        if isinstance(times, np.ndarray):
-            times = times.tolist()
-        # If a list, we do need Python builtins instead of np data types due to database requirements
-        if isinstance(times, list):
-            if not isinstance(times[0], int):
-                try:
-                    times = [int(x) for x in times]
-                except:
-                    msg = "Parameter times includes elements that could not be cast to integer"
-                    logging.error(msg)
-                    raise AttributeError(msg)
-        if isinstance(times, dict):
-            for key in times:
-                try:
-                    times[key] = int(times[key])
-                except:
-                    msg = "Parameter times includes elements that could not be cast to integer"
-                    logging.error(msg)
-                    raise AttributeError(msg)
-
-    if not isinstance(tokens, (np.ndarray, list, int, np.integer, np.str_, np.string_)):
-        logging.error("Token parameter should be string, int or list. Supplied: {}".format(type(tokens)))
-        raise AttributeError("Token parameter should be string, int or list. Supplied: {}".format(type(tokens)))
-    else:
-        # We use lists instead of arrays here
-        if isinstance(tokens, np.ndarray):
-            tokens = times.tolist()
-        # If a list, we do need Python builtins instead of np data types due to database requirements
-        if isinstance(tokens, list):
-            for i, token in enumerate(tokens):
-                if isinstance(token, (np.integer)):
-                    tokens[i] = int(token)
-                elif isinstance(token, (np.str_, np.string_)):
-                    tokens[i] = str(token)
-                elif not isinstance(token, (str, int)):
-                    msg = "Parameter times includes elements that could not be cast to integer"
-                    logging.error(msg)
-                    raise AttributeError(msg)
-        if isinstance(times, dict):
-            for key in times:
-                try:
-                    times[key] = int(times[key])
-                except:
-                    msg = "Parameter times includes elements that could not be cast to integer"
-                    logging.error(msg)
-                    raise AttributeError(msg)
+    if times is not None:
+        if not isinstance(times, (np.ndarray, list, dict, int, np.integer)):
+            logging.error(
+                "Parameter times must be int, list, array or interval dict ('start':int,'end':int). Supplied: {}".format(
+                    type(times)))
+            raise AttributeError(
+                "Parameter times must be int, list, array or interval dict ('start':int,'end':int). Supplied: {}".format(
+                    type(times)))
+        else:
+            # We use lists instead of arrays here
+            if isinstance(times, np.ndarray):
+                times = times.tolist()
+            # If a list, we do need Python builtins instead of np data types due to database requirements
+            if isinstance(times, list):
+                if not isinstance(times[0], int):
+                    try:
+                        times = [int(x) for x in times]
+                    except:
+                        msg = "Parameter times includes elements that could not be cast to integer"
+                        logging.error(msg)
+                        raise AttributeError(msg)
+            if isinstance(times, dict):
+                for key in times:
+                    try:
+                        times[key] = int(times[key])
+                    except:
+                        msg = "Parameter times includes elements that could not be cast to integer"
+                        logging.error(msg)
+                        raise AttributeError(msg)
+    if token is not None:
+        if not isinstance(tokens, (np.ndarray, list, int, np.integer, np.str_, np.string_)):
+            logging.error("Token parameter should be string, int or list. Supplied: {}".format(type(tokens)))
+            raise AttributeError("Token parameter should be string, int or list. Supplied: {}".format(type(tokens)))
+        else:
+            # We use lists instead of arrays here
+            if isinstance(tokens, np.ndarray):
+                tokens = times.tolist()
+            # If a list, we do need Python builtins instead of np data types due to database requirements
+            if isinstance(tokens, list):
+                for i, token in enumerate(tokens):
+                    if isinstance(token, (np.integer)):
+                        tokens[i] = int(token)
+                    elif isinstance(token, (np.str_, np.string_)):
+                        tokens[i] = str(token)
+                    elif not isinstance(token, (str, int)):
+                        msg = "Parameter times includes elements that could not be cast to integer"
+                        logging.error(msg)
+                        raise AttributeError(msg)
+            if isinstance(times, dict):
+                for key in times:
+                    try:
+                        times[key] = int(times[key])
+                    except:
+                        msg = "Parameter times includes elements that could not be cast to integer"
+                        logging.error(msg)
+                        raise AttributeError(msg)
 
     if tokens is not None and times is not None:
         return times, tokens
